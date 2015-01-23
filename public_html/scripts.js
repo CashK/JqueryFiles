1da8270a1501f39650124fb5ed0eb8914db1cd34
Kobi Cash <bluetorrent6@gmail.com>
2015-01-23 07:55:01 -0800

    stuff

--- public_html/scripts.js
+++ public_html/scripts.js
@@ -71,3 +71,89 @@ $("document").ready(function() {
     }); 
 });
 //Video 2
+
+//Video 3
+$("document").ready(function() {
+    
+    $('#oneButton').bind('click', alertButtonClick);
+    
+    $('(#textBox1)').bind('blur', onBLurEvent)
+                    .bind('focus', onFocusEvent)
+                    .bind('onmousedown', onMDownEvent)
+                    .bind('onmouseup', onMUpEvent)
+                    .bind('change', onChangeEvent)
+            
+    $(window).resize(resizedWindow);
+    
+    $('#logo').bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
+    
+    $('#twoButton').bind('dblclick', dblClickedMe);
+    
+    $('form').submit(function() { alert("Submit button clicked") });
+    
+    $('#threeButton').bind('click', unbindLogo);
+    
+    $('#theBody').bind('keyup', checkKeyPressed).bind('mousemove', 
+    theMouseMoved).click(event,eventTriggered);
+    });
+    
+    function alertButtonClick() {
+        alert("There was a button clicked");
+    }
+    
+    function onBlurEvent() {
+        $("#second").html("You left the box");
+    }
+    
+    function onFocusEvent() {
+        $("#second").html("you entered the box");
+    }
+    
+    function onMDownEvent() {
+        $("#second").html("you left the textbox");
+    }
+    
+    function onMUpEvent() {
+        $("#second").html("you entered the box");
+    }
+    
+    function onChangeEvent() {
+        $("#second").html("you changed the box");
+    }
+    
+    function resizedWindow() {
+        $("#second").html("Window was resized W: " + $(window).width() + "H:" +
+                $(window).height());
+    }
+    
+    
+    function mouseOverMe() {
+        $("#second").html("You put your cursor on my logo");
+    }
+    
+    function mouseOutMe() {
+        $("#second").html("You stopped touching my logo");
+    }
+    
+    function onFocusEvent() {
+        $("#second").html("You double clicked my button");
+    }
+    
+    function unbindLogo() {
+        $("#logo").unbind('mouseover', mouseOverMe).unbind('mouseout', mouseOut);
+    }
+    
+    function checkKeyPressed(event) {
+        $("#fifth").text(String.fromCharCode(event.keyCode));
+    }
+    
+    function theMouseMoved(event) {
+        $("#seventh").html(event.screenX);
+        $(#ninth).html(event.screenY);
+    }
+    
+    function eventTriggered(event) {
+        $("#tenth").html(event.target.nodeName);
+        $("eleventh").html(event.timeStamp);
+    }
+//Video 3
